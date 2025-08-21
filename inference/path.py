f831a0d625722357377fff2a2ca38558ec38e93f
molba2see <molba2see@gmail.com>
2025-08-21 15:30:06 +0900

    경로 수정

--- inference/path.py
+++ inference/path.py
@@ -1,15 +1,15 @@
 import os, sys
 
 paths = {
-    "PIP_CACHE_DIR": "/cache/pip",
-    "PYTHONUSERBASE": "/.local_lib",
-    "HF_HOME": "/hf",
-    "TRANSFORMERS_CACHE": "/cache/hf_transformers",
-    "HF_DATASETS_CACHE": "/cache/hf_datasets",
-    "TORCH_HOME": "/cache/torch",
-    "XDG_CACHE_HOME": "/cache",
-    "CUDA_CACHE_PATH": "/cache/ccache",
-    "TMPDIR": "/tmp",
+    "PIP_CACHE_DIR": "cache/pip",
+    "PYTHONUSERBASE": ".local_lib",
+    "HF_HOME": "hf",
+    "TRANSFORMERS_CACHE": "cache/hf_transformers",
+    "HF_DATASETS_CACHE": "cache/hf_datasets",
+    "TORCH_HOME": "cache/torch",
+    "XDG_CACHE_HOME": "cache",
+    "CUDA_CACHE_PATH": "cache/ccache",
+    "TMPDIR": "tmp",
 }
 
 # 환경변수 적용
@@ -22,5 +22,5 @@ for k, v in paths.items():
 # PYTHONPATH 확장
 sys.path.insert(
     0,
-    "/.local_lib/lib/python{sys.version_info.major}.{sys.version_info.minor}/site-packages"
+    ".local_lib/lib/python{sys.version_info.major}.{sys.version_info.minor}/site-packages"
 )
